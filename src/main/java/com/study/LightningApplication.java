a01772a806762c30ec7b3b4b420ba1e4fcbaf2d0
david <271925608@qq.com>
2017-09-01 14:14:03 +0800

    提交信息

---
+++ src/main/java/com/study/LightningApplication.java
@@ -0,0 +1,12 @@
+package com.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LightningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LightningApplication.class, args);
+	}
+}
